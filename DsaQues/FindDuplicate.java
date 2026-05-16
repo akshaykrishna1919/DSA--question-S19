b6a069aa239d410f28b09a3367471b9504f8d1a9
Akshay Krishana <akshaykrishna6p@gmai.com>
2026-05-16 23:58:37 +0530

    Added find duplicate element DSA solution related to #8

---
+++ DsaQues/FindDuplicate.java
@@ -0,0 +1,31 @@
+
+
+import java.util.HashSet;
+
+public class FindDuplicate {
+
+    public static int findDuplicate(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+
+            if (set.contains(num)) {
+                return num;
+            }
+
+            set.add(num);
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 3, 4, 2, 2};
+
+        int result = findDuplicate(nums);
+
+        System.out.println("Duplicate Element: " + result);
+    }
+}
