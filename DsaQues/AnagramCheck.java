2b57afabe19b98f367b2b55a2f6f4565ce61bdd1
Akshay Krishana <akshaykrishna6p@gmai.com>
2026-07-30 12:50:22 +0530

    Added Anagram Check DSA solution, related to #19

---
+++ DsaQues/AnagramCheck.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Arrays;
+
+public class AnagramCheck {
+
+    public static boolean isAnagram(String s1, String s2) {
+
+        char[] a = s1.toLowerCase().toCharArray();
+        char[] b = s2.toLowerCase().toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isAnagram("listen", "silent"));
+    }
+}
